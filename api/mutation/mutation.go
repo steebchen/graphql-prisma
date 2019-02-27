6c8bd4df3ae7128e79535e121b5f7b6eedb3ad71
Jonathan Scholz <contact@jonathan-scholz.de>
2019-02-27 17:18:28 +0100

    feat(auth): add automatic session cookie renewal on every request

--- api/mutation/mutation.go
+++ api/mutation/mutation.go
@@ -3,6 +3,7 @@ package mutation
 import (
 	"github.com/steebchen/graphql/api/mutation/auth"
 	"github.com/steebchen/graphql/api/super"
+	"github.com/steebchen/graphql/gqlgen"
 )
 
 type Mutation struct {
@@ -10,7 +11,7 @@ type Mutation struct {
 	*auth.Auth
 }
 
-func New(super *super.Resolver) *Mutation {
+func New(super *super.Resolver) gqlgen.MutationResolver {
 	return &Mutation{
 		Resolver: super,
 		Auth:     auth.New(super),
--- api/resolver.go
+++ api/resolver.go
@@ -8,16 +8,16 @@ import (
 	"github.com/steebchen/graphql/prisma"
 )
 
+type Resolver struct {
+	*super.Resolver
+}
+
 func New(client *prisma.Client) *Resolver {
 	return &Resolver{
 		Resolver: &super.Resolver{
 			Prisma: client,
 		},
 	}
-}
-
-type Resolver struct {
-	*super.Resolver
 }
 
 func (r *Resolver) Mutation() gqlgen.MutationResolver {
